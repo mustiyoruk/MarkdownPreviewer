d1325a95a84b3407ffc2665e5bae53b3307fd69c
mustiyoruk <mustiyoruk@yahoo.com>
2022-09-12 12:23:09 +0300

    text previewer is created

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 function App() {
-  return <div className="text-center">helo world for mustafa</div>;
+  const [textInput, setTextInput] = React.useState("");
+
+  return (
+    <div className="text-center px-4">
+      <h1 className="px-4">Markdown Previewer</h1>
+      <textarea
+        name="textInput"
+        id=""
+        cols="30"
+        rows="10"
+        className="textarea"
+        value={textInput}
+      ></textarea>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
